a0fb53672a25a4bff3b154df5bff0a34fd787547
zzzzzzzs <1443539042@qq.com>
2023-06-29 14:28:02 +0000

    add ENABLE_FACTORY_CREATOR links

--- src/smart_point/factory_creator_test.cpp
+++ src/smart_point/factory_creator_test.cpp
@@ -1,6 +1,8 @@
 #include "factory_creator.h"
 #include <iostream>
 
+
+// TODO 参考 https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=240883637
 class MyClass {
 private:
     ENABLE_FACTORY_CREATOR(MyClass)
@@ -23,7 +25,7 @@ int main(){
     // 通过 create_unique 创建对象
     std::unique_ptr<MyClass> obj2 = MyClass::create_unique();
     obj2->DoSomething();
-    
+
     // MyClass* obj3 = new MyClass();  // 错误！无法直接调用 new 运算符创建对象
 
 }
