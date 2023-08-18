ee15c47799011cb3b0f3b52c9d83f3a34d88bcb1
zzzzzzzs <1443539042@qq.com>
2023-08-18 10:09:13 +0000

    1

--- src/debug_info/debug_info.cpp
+++ src/debug_info/debug_info.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
 //宏前面加上##的作用在于：当可变参数的个数为0时，这里的## 起到把前面多余的","去掉的作用
-#define DEBUG_INFO(format, ...) printf("File:%s, Line:%d, Function:%s, %s\n", \
-	__FILE__, __LINE__ , __FUNCTION__, ##__VA_ARGS__);
+// #define DEBUG_INFO(format, ...) printf("File:%s, Line:%d, Function:%s, %s\n", \
+// 	__FILE__, __LINE__ , __FUNCTION__, ##__VA_ARGS__);
+
+#define DEBUG_INFO(format, ...) \
+    printf("File:%s, Line:%d, Function:%s, " format "\n", \
+    __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__);
 
 int main(){
     DEBUG_INFO("%s", "hello world");
